09fcd8570fefdb6af8e918712c5188c922784652
studensk <kala.studens@gmail.com>
2024-07-19 15:59:40 -0400

    create met directory if needed

--- R/hysplit_trajectory.R
+++ R/hysplit_trajectory.R
@@ -22,14 +22,18 @@ download_met_files <- function(days,
                                direction,
                                met_dir = paste0(getwd(), '/meteorology')) {
   options(timeout = 800)
-  
+
+  if (!dir.exists(met_dir)) {
+    dir.create(met_dir)
+  }
+
   metfiles <- paste0(met_dir, '/', list.files(met_dir))
   metfile_sizes <- file.info(metfiles)$size
   true_sizes <- sort(unique(metfile_sizes), decreasing = TRUE)[1:2]
   min_size <- min(true_sizes)
   small_file_inds <- which(metfile_sizes < min_size)
   file.remove(metfiles[small_file_inds])
-  
+
   get_daily_filenames(
     days = days,
     duration = duration,
@@ -40,13 +44,13 @@ download_met_files <- function(days,
       path_met_files = met_dir,
       ftp_dir = "ftp://arlftp.arlhq.noaa.gov/archives/nams"
     )
-  
+
 }
 
 ## Hysplit Runs ##
 ## run_df should have lat, lon, height, date and hour columns. If not in GMT,
 ##   there should also be a "timezone" column
-## 
+##
 ## if not using run_df, it's assumed that all of the times are in GMT
 
 hysplit_trajectory <- function(run_df = NULL,
@@ -68,42 +72,42 @@ hysplit_trajectory <- function(run_df = NULL,
                                exec_dir = NULL,
                                clean_up = TRUE,
                                local_time = FALSE) {
-  
-  
-  
+
+
+
   # If the execution dir isn't specified, use the working directory
   if (is.null(exec_dir)) exec_dir <- getwd()
-  
+
   # If the meteorology dir isn't specified, use the working directory
   if (is.null(met_dir)) met_dir <- paste0(getwd(), '/meteorology')
-  
+
   if (!is.null(run_df)) {days <- unique(run_df$date)}
-  
+
   days <- as.Date(days)
-  
+
   all_met_files <- list.files(met_dir)
   met_file_check <- get_daily_filenames(
     days, duration, direction, suffix = "_hysplit.t00z.namsa"
   )
-  
+
   infolder <- met_file_check %in% all_met_files
   if (!all(infolder)) {
     w <- which(!(infolder))
-    stop('Missing the following met files: \n\n', 
-         paste(met_file_check[w], collapse = '\n'), 
+    stop('Missing the following met files: \n\n',
+         paste(met_file_check[w], collapse = '\n'),
          '\n\nUse download_met_files()')
   }
-  
+
   # Set the path for the `hyts_std` binary file
-  binary_path <- 
+  binary_path <-
     set_binary_path(
       binary_path = binary_path1,
       binary_name = "hyts_std"
     )
-  
+
   # Get the system type
   system_type <- get_os()
-  
+
   # Generate name of output folder
   if (is.null(traj_name)) {
     folder_name <- paste0("traj-", format(Sys.time(), "%Y-%m-%d-%H-%M-%S"))
@@ -111,23 +115,23 @@ hysplit_trajectory <- function(run_df = NULL,
     folder_name <- traj_name
   }
   recep_file_path <- file.path(exec_dir, folder_name)
-  
+
   if (is.null(config)) {
     config_list <- set_config()
   } else {
     config_list <- config
   }
-  
+
   if (is.null(ascdata)) {
     ascdata_list <- set_ascdata()
   } else {
     ascdata_list <- ascdata
   }
-  
+
   # Modify the default `SETUP.CFG` file when the option for extended
   # meteorology is `TRUE`
   if (isTRUE(extended_met)) {
-    
+
     tm_names <-
       config_list %>%
       names() %>%
@@ -137,7 +141,7 @@ hysplit_trajectory <- function(run_df = NULL,
         FUN = function(y) y %>% tidy_grepl("^tm_")
       ) %>%
       which()
-    
+
     config_list[tm_names] <- 1
   }
   print('met cleared')
@@ -145,55 +149,55 @@ hysplit_trajectory <- function(run_df = NULL,
   # the `exec` directory
   config_list %>% write_config_list(dir = exec_dir)
   ascdata_list %>% write_ascdata_list(dir = exec_dir)
-  
+
   # Stop function if there are vectors of different
   # length for `lat` and `lon`
   if (length(lat) != length(lon)) {
     stop("The coordinate vectors are not the same length.", call. = FALSE)
   }
-  
+
   # Generate a tibble of receptor sites
   if (is.null(run_df)) {
-    receptors_tbl <- 
+    receptors_tbl <-
       dplyr::tibble(lat = lat, lon = lon) %>%
-      dplyr::group_by(lat, lon) %>% 
+      dplyr::group_by(lat, lon) %>%
       tidyr::expand(height = height, hour = daily_hours, date = days) %>%
       dplyr::ungroup() %>%
       dplyr::mutate(receptor = dplyr::row_number()) %>%
       dplyr::select(receptor, dplyr::everything())
   }
-  
+
   else {
     receptors_tbl <- run_df %>%
       dplyr::as_tibble() %>%
       dplyr::mutate(receptor = dplyr::row_number()) %>%
       dplyr::select(receptor, dplyr::everything())
-      
+
   }
-  
+
   if (local_time) {
-    receptors_tbl$timezone <- tz_lookup_coords(lat = receptors_tbl$Latitude, 
-                                               lon = receptors_tbl$Longitude, 
+    receptors_tbl$timezone <- tz_lookup_coords(lat = receptors_tbl$Latitude,
+                                               lon = receptors_tbl$Longitude,
                                                method = 'accurate')
     dt <- as.POSIXct(paste0(receptors_tbl$date, '00:00'),
                      format = '%Y-%m-%d %H:%M',
                      tz = receptors_tbl$timezone)
     new_dt <- with(new, 'GMT')
   }
-  
+
   # Get vector of receptor indices
   receptors <- seq(nrow(receptors_tbl))
-  
+
   cores <- parallel::detectCores()
   max_clusters <- floor(cores*2/3)
   clusters <- pmin(max_clusters, max(receptors))
   print(paste0(clusters, ' clusters'))
-  
+
   print('initialize clusters')
   cl <- makeCluster(clusters)
   clusterEvalQ(cl, {
     library(tidyverse)
-    
+
     source('code/util_file.R')
     source('code/met_utils.R')
   })
@@ -201,53 +205,53 @@ hysplit_trajectory <- function(run_df = NULL,
                       'exec_dir', 'duration', 'direction',
                       'traj_name', 'vert_motion', 'model_height', 'receptors',
                       'system_type', 'met_dir', 'binary_path', 'folder_name',
-                      'config_list', 'ascdata_list', 'recep_file_path'), 
+                      'config_list', 'ascdata_list', 'recep_file_path'),
                 envir = environment())
   print('clusters initialized')
   traj.lst <- parLapply(cl, receptors, function(receptor) {
-    
+
     inner_folder <- paste0('receptor', receptor)
     inner_dir <- file.path(exec_dir, inner_folder)
     if (!dir.exists(inner_dir)) {
       dir.create(inner_dir)
     }
-    
+
     config_list %>% write_config_list(dir = file.path(inner_dir))
     ascdata_list %>% write_ascdata_list(dir = file.path(inner_dir))
-    
-    receptor_vals <- 
+
+    receptor_vals <-
       get_receptor_values(
         receptors_tbl = receptors_tbl,
         receptor_i = receptor
       )
-    
+
     receptor_i <- receptor_vals$receptor
     lat_i <- receptor_vals$lat
     lon_i <- receptor_vals$lon
     height_i <- receptor_vals$height
     date_i <- receptor_vals$date
     hour_i <- receptor_vals$hour
-    
-    met_files <- 
+
+    met_files <-
       get_daily_filenames(
         as.Date(date_i), duration, direction, suffix = "_hysplit.t00z.namsa"
       )
-    
+
     #list_run_days <- days %>% as.character()
-    
+
     start_year_GMT <- to_short_year(date_i)
     start_month_GMT <- to_short_month(date_i)
     start_day_GMT <- to_short_day(date_i)
-    
+
     # Sort daily starting hours if given as
     # numeric values
     if (inherits(hour_i, "numeric")) {
       hour_i <- formatC(sort(hour_i), width = 2, flag = 0)
     }
-    
+
     start_hour_GMT <- hour_i
     full_year_GMT <- as.character(year(as.Date(date_i)))
-    
+
     output_filename <-
       get_traj_output_filename(
         traj_name = traj_name,
@@ -262,8 +266,8 @@ hysplit_trajectory <- function(run_df = NULL,
         height = height_i,
         duration = duration
       )
-    
-    
+
+
     # Write the CONTROL file
     write_traj_control_file(
       start_year_GMT = start_year_GMT,
@@ -283,10 +287,10 @@ hysplit_trajectory <- function(run_df = NULL,
       met_dir = met_dir,
       exec_dir = inner_dir
     )
-    
+
     # The CONTROL file is now complete and in the
     # working directory, so, execute the model run
-    sys_cmd <- 
+    sys_cmd <-
       paste0(
         "(cd \"",
         inner_dir,
@@ -296,15 +300,15 @@ hysplit_trajectory <- function(run_df = NULL,
         to_null_dev(system_type = system_type),
         ")"
       )
-    
+
     execute_on_system(sys_cmd, system_type = system_type)
-    
-    
+
+
     #recep_file_path <- file.path(exec_dir, folder_name)
     if (!dir.exists(recep_file_path)) {
       dir.create(path = recep_file_path, recursive = TRUE)
     }
-    
+
     # Move files into the output folder
     trajectory_file <- file.path(inner_dir, output_filename)
     file.copy(
@@ -312,13 +316,13 @@ hysplit_trajectory <- function(run_df = NULL,
       to = recep_file_path,
       copy.mode = TRUE
     )
-    
+
     unlink(inner_dir, force = TRUE, recursive = TRUE)
   })
   stopCluster(cl)
-  
+
   print('parallel computing complete')
-  
+
   # For every set of coordinates, perform a set
   # of model runs
   # Obtain a trajectory data frame
@@ -331,11 +335,11 @@ hysplit_trajectory <- function(run_df = NULL,
       lon_i = lon_i,
       height_i = height_i
     )
-  
+
   if (clean_up) {
     unlink(recep_file_path, recursive = TRUE, force = TRUE)
   }
-  
+
   ensemble_tbl <-
     traj_tbl %>%
     dplyr::select(-c(year, month, day, hour)) %>%
@@ -356,20 +360,20 @@ hysplit_trajectory <- function(run_df = NULL,
       receptor, hour_along, traj_dt, traj_dt_i, lat_i, lon_i, height_i) %>%
     dplyr::slice(1) %>%
     dplyr::ungroup()
-  
+
   if (direction == "forward") {
-    
+
     ensemble_tbl <-
       ensemble_tbl %>%
       dplyr::arrange(receptor, traj_dt_i)
-    
+
   } else {
-    
+
     ensemble_tbl <-
       ensemble_tbl %>%
       dplyr::arrange(receptor, traj_dt_i, dplyr::desc(hour_along))
   }
-  
+
   ensemble_tbl %>%
     dplyr::right_join(
       ensemble_tbl %>%
